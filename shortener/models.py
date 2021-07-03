2cc27b51fee51f0be66208db445f355a9ec21430
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-07-03 09:57:31 -0300

    create simple Link model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Link(models.Model):
+    url = models.CharField(max_length=100)
+    shortened_url = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=timezone.now(), editable=False)
+    # TODO: relation with User model
+    created_by = models.CharField(max_length=100)

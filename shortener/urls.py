3ba6003ca6dbb7a8c1cc0214396e41e00cf04b13
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-07-05 09:30:08 -0300

    add links table to home using links route

--- shortener/urls.py
+++ shortener/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('links/', views.LinksView.as_view()),
+    path('links/', views.LinksView.as_view(), name='links'),
     path('shortener/', views.ShortenerView.as_view())
 ]

62e9141e47780d8983b7808c15af451b3afc0506
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-07-04 21:20:11 -0300

    add signup route and template

--- project/urls.py
+++ project/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.SignUpView.as_view(), name='signup'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('api/', include('shortener.urls'))
 ]
